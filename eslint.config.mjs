521fff7bc8e143c56cebc3ffd98b68b630c3a433
CATL <3401075539@qq.com>
2025-06-13 17:23:44 +0800

    fix: 完善eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
+  ),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ];
 
 export default eslintConfig;
