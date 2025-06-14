138ad401bdd43365f3b89a85e4e3a28ccf595190
KanJolin <Kanjolin@hotmail.com>
2025-06-14 22:32:50 +0800

    fix: 去除google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 // src/app/layout.tsx
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { ConfigProvider } from 'antd';
 import theme from './themeConfig';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '协同文档系统',
@@ -21,11 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      </head>
-      <body className={inter.className}>
+      <body>
         <AntdRegistry>
           <ConfigProvider theme={theme}>
             {children}
